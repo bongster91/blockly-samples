15cb634813caa2ddfd200d10641a484946fef0fb
root <root@NYC-BXTI-L932.Blackstone.com>
2023-02-27 09:08:52 -0500

    build a custom generator blockly codelab complete

---
+++ examples/custom-toolbox-codelab/starter-code/custom_category.js
@@ -0,0 +1,20 @@
+class CustomCategory extends Blockly.ToolboxCategory {
+    /**
+     * Constructor for a custom category.
+     * @override
+     */
+    constructor(categoryDef, toolbox, opt_parent) {
+        super(categoryDef, toolbox, opt_parent);
+    };
+
+    /** @override */
+    addColourBorder_(colour){
+        this.rowDiv_.style.backgroundColor = colour;
+    };
+};
+
+Blockly.registry.register(
+    Blockly.registry.Type.TOOLBOX_ITEM,
+    Blockly.ToolboxCategory.registrationName,
+    CustomCategory, true
+);
